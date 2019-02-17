8facfa5dd0d748009c67955fd2760eedc5cca1c0
Senni Kaipio <senni.kaipio@protonmail.com>
2019-02-17 11:44:02 +0200

    Refactor tests into describe blocks

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -16,79 +16,83 @@ beforeEach(async () => {
   await Promise.all(promiseArray)
 })
 
-test('GET /api/blogs returns all blogs', async () => {
-  const response = await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-
-  expect(response.body.length).toBe(blogMocks.blogs.length)
+describe('GET /api/blogs', () => {
+  test('returns all blogs', async () => {
+    const response = await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  
+    expect(response.body.length).toBe(blogMocks.blogs.length)
+  })
+  
+  test('returns blogs with id fields', async () => {
+    const response = await api
+      .get('/api/blogs')
+  
+    const blog = response.body[0]
+  
+    expect(blog.id).toBeDefined()
+  })
 })
 
-test('returned blogs have id field', async () => {
-  const response = await api
-    .get('/api/blogs')
-
-  const blog = response.body[0]
-
-  expect(blog.id).toBeDefined()
-})
-
-test('POST /api/blogs saves a new note', async () => {
-  await api
-    .post('/api/blogs')
-    .send(blogMocks.singleBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
-
-  const response = await api
-    .get('/api/blogs')
-
-  const blogsWithoutIds = response.body.map(blog => {
-    const blogWithoutId = {...blog}
-    delete blogWithoutId.id
-    return blogWithoutId
+describe('POST /api/blogs', () => {
+  test('saves a new note', async () => {
+    await api
+      .post('/api/blogs')
+      .send(blogMocks.singleBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+  
+    const response = await api
+      .get('/api/blogs')
+  
+    const blogsWithoutIds = response.body.map(blog => {
+      const blogWithoutId = {...blog}
+      delete blogWithoutId.id
+      return blogWithoutId
+    })
+  
+    expect(response.body.length).toBe(blogMocks.blogs.length + 1)
+    expect(blogsWithoutIds).toContainEqual(blogMocks.singleBlog)
   })
 
-  expect(response.body.length).toBe(blogMocks.blogs.length + 1)
-  expect(blogsWithoutIds).toContainEqual(blogMocks.singleBlog)
-})
+  test('returns 400 Bad Request if title or url is not set', async () => {
+    const blogWithoutTitle = {...blogMocks.singleBlog}
+    delete blogWithoutTitle.title
+  
+    await api
+      .post('/api/blogs')
+      .send(blogWithoutTitle)
+      .expect(400)
+  
+    const blogWithoutUrl = {...blogMocks.singleBlog}
+    delete blogWithoutUrl.url
+  
+    await api
+      .post('/api/blogs')
+      .send(blogWithoutUrl)
+      .expect(400)
+  })
 
-test('POST /api/blogs returns 400 Bad Request if title or url is not set', async () => {
-  const blogWithoutTitle = {...blogMocks.singleBlog}
-  delete blogWithoutTitle.title
-
-  await api
-    .post('/api/blogs')
-    .send(blogWithoutTitle)
-    .expect(400)
-
-  const blogWithoutUrl = {...blogMocks.singleBlog}
-  delete blogWithoutUrl.url
-
-  await api
-    .post('/api/blogs')
-    .send(blogWithoutUrl)
-    .expect(400)
-})
-
-test('blog likes field is by default set to 0', async () => {
-  const blogWithoutLikes = {...blogMocks.singleBlog}
-  delete blogWithoutLikes.likes
-
-  const newBlogResponse = await api
-    .post('/api/blogs')
-    .send(blogWithoutLikes)
-
-  const { body } = await api
-    .get('/api/blogs')
-
-  for (const blog of body) {
-    expect(blog.likes).toBeDefined()
-    if (blog.id === newBlogResponse.id) {
-      expect(blog.likes).toBe(0)
+  test("sets blog likes field to 0 if it isn't defined", async () => {
+    const blogWithoutLikes = {...blogMocks.singleBlog}
+    delete blogWithoutLikes.likes
+  
+    const newBlogResponse = await api
+      .post('/api/blogs')
+      .send(blogWithoutLikes)
+  
+    const { body } = await api
+      .get('/api/blogs')
+  
+    for (const blog of body) {
+      expect(blog.likes).toBeDefined()
+      if (blog.id === newBlogResponse.id) {
+        expect(blog.likes).toBe(0)
+      }
     }
-  }
+  })
 })
 
 afterAll(async () => {
